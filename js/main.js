a8db8d33b2930aa55a8e2e7334031e5a2305d775
emilylavinskas <emily.ann28@hotmail.com>
2015-03-03 10:20:44 -0500

    added js and css
    added functions in javascript, and styled list with main.css and
    typography.css

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-var $form = $ ('form');
+var $form = $('.form');
+var $name = $('.main-input');
+var $list = $('.list');
 
+$form.on('submit', function (e) {
+    e.preventDefault();
+       
+    var $li = $('<li>').html($name.val());
+    var $nameDl = $('<button class="delete">x</button>')
+
+    $nameDl.on('click', function () {
+        $li.remove('li');
+    });
+
+    $list.on('click', 'li', function () {
+        $(this).addClass('complete');
+    });
+
+    $li.append($nameDl);
+    $list.prepend($li);
+    $name.val('');
+
+});
